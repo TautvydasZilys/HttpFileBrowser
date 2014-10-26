65ef88f8e6fcc1527e70a6ba2df1fecb6de95445
Sunius <suniusx@gmail.com>
2014-10-26 17:17:17 +0200

    Gah.

--- RemoteFileBrowser/RemoteFileBrowser/Utilities/Utilities.cpp
+++ RemoteFileBrowser/RemoteFileBrowser/Utilities/Utilities.cpp
@@ -303,12 +303,10 @@ void Encoding::EncodeUrlInline(string& url)
 	}
 }
 
-static char s_Base64Table1[64] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-								  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
-								  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-								  'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '9', '+', '/' };
-
-static vector<char> s_Base64Table(s_Base64Table1, s_Base64Table1 + 64);
+static const char s_Base64Table[64] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+										  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 
+										  'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
+										  'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' };
 
 void Encoding::EncodeBase64Inline(std::string& data)
 {
