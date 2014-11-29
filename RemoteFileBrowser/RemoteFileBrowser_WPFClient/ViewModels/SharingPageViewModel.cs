f80c699576ff3aa51de9f0e65d2f95cf52095718
Sunius <suniusx@gmail.com>
2014-11-29 22:12:01 +0200

    Sharing page design.

--- RemoteFileBrowser/RemoteFileBrowser_WPFClient/ViewModels/SharingPageViewModel.cs
+++ RemoteFileBrowser/RemoteFileBrowser_WPFClient/ViewModels/SharingPageViewModel.cs
@@ -11,6 +11,11 @@ namespace RemoteFileBrowser.ViewModels
     class SharingPageViewModel : INotifyPropertyChanged
     {
         private bool m_IsSharing;
+        private string m_PublicName;
+        private string m_HostId;
+        private bool m_AllowDirectConnections;
+        private bool m_EnableMulticast;
+        private bool m_RequireAuthentification = true;
 
         public bool IsSharing
         {
@@ -24,7 +29,48 @@ namespace RemoteFileBrowser.ViewModels
         }
 
         public string SharingStatusString { get { return IsSharing ? "Running" : "Stopped"; } }
+        public string HostId { get { return m_HostId; } }
 
+        public string PublicName
+        {
+            get { return m_PublicName; }
+            set
+            {
+                m_PublicName = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool AllowDirectConnections
+        {
+            get { return m_AllowDirectConnections; }
+            set
+            {
+                m_AllowDirectConnections = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool EnableMulticast
+        {
+            get { return m_EnableMulticast; }
+            set
+            {
+                m_EnableMulticast = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public bool RequireAuthentification
+        {
+            get { return m_RequireAuthentification; }
+            set
+            {
+                m_RequireAuthentification = value;
+                NotifyPropertyChanged();
+            }
+        }
+        
         public event PropertyChangedEventHandler PropertyChanged;
         private void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
         {
