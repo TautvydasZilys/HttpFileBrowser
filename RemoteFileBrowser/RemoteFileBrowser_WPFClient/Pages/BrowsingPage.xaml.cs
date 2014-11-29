5204db553f07cdf5b08e0ff4762f38fc86f2d895
Sunius <suniusx@gmail.com>
2014-11-29 20:59:59 +0200

    Add untracked files.

---
+++ RemoteFileBrowser/RemoteFileBrowser_WPFClient/Pages/BrowsingPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace RemoteFileBrowser.Pages
+{
+    public partial class BrowsingPage : Page
+    {
+        public BrowsingPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RemoteFileBrowser/RemoteFileBrowser_WPFClient/Pages/SharingPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using RemoteFileBrowser.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RemoteFileBrowser.Pages
+{
+    public partial class SharingPage : Page
+    {
+        SharingPageViewModel m_ViewModel;
+
+        public SharingPage()
+        {
+            InitializeComponent();
+            DataContext = m_ViewModel = new SharingPageViewModel();
+        }
+
+        private void StartSharingButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_ViewModel.IsSharing)
+                throw new InvalidOperationException();
+
+            m_ViewModel.IsSharing = true;
+        }
+
+        private void StopSharingButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!m_ViewModel.IsSharing)
+                throw new InvalidOperationException();
+
+            m_ViewModel.IsSharing = false;
+        }
+    }
+}
